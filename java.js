7582c6008c2c184ba001d8797e64795eb00fe4e4
MoraisL <viniciusmoraaais@gmail.com>
2023-06-12 21:28:26 -0300

    modificações

---
+++ java.js
@@ -0,0 +1,29 @@
+
+var slideIndex = 1;
+showSlide(slideIndex);
+
+function prevSlide() {
+    showSlide(slideIndex -= 1);
+}
+
+function nextSlide() {
+    showSlide(slideIndex += 1);
+}
+
+function showSlide(n) {
+    var slides = document.getElementsByClassName("slide");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
