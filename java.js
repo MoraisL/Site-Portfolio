b27907f392844fc6b090c004ae1c1ce571294d6a
MoraisL <viniciusmoraaais@gmail.com>
2023-06-14 16:48:48 -0300

    complet

--- java.js
+++ java.js
@@ -28,12 +28,25 @@ function showSlide(n) {
     slides[slideIndex - 1].style.display = "block";
 }
 
-function changePage() {
-    const targetId = "LinguagensId";
-    const content = document.getElementById(targetId);
-    if (content) {
-      content.scrollIntoView({ behavior: 'smooth' });
+window.onscroll = function() {
+    mostrarBotao();
+  };
+
+  function mostrarBotao() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("voltarTopo").style.display = "block";
     } else {
-      console.log("ID não encontrada!");
+      document.getElementById("voltarTopo").style.display = "none";
+    }
+  }
+
+  function voltarAoTopo() {
+    // Verifica a compatibilidade do navegador
+    if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
+      // Usa o método 'scrollTo' para uma transição suave
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
     }
   }
